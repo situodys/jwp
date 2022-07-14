38a4085ab2ad3cd5b5f2eedddd2a0d5c0dbda81b
situodys <situodys@gmail.com>
2022-07-14 16:36:19 +0900

    문자열 계산기 완료

--- src/test/java/chap02/calculator/StringCalculator.java
+++ src/test/java/chap02/calculator/StringCalculator.java
@@ -1,16 +1,60 @@
 package chap02.calculator;
 
-public class StringCalculator {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class StringCalculator {
+    /*
+    1. 메서드가 한가지 책임만 갖도록 구현
+    2. 인덴트를 1로 유지
+    3. else 사용하지 않기
+     */
 
     public int sum(String s) {
+        if(isBlank(s))
+            return 0;
+        return sum(toInts(split(s)));
+    }
 
-        int result=0;
-        String[] values = s.split(",");
+    private boolean isBlank(String s) {
+        return s.isEmpty() || s == null;
+    }
 
-        for (String value : values) {
-            result += Integer.parseInt(value);
+    private int sum(int[] numbers) {
+        int sum=0;
+        for (int number : numbers) {
+            sum+=number;
         }
-        return result;
+        return sum;
+    }
+
+    private String[] split(String s) {
+        String delimiter=":|,";
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        if(m.find()){
+            delimiter +="|";
+            delimiter +=m.group(1);
+            return m.group(2).split(delimiter);
+        }
+        return s.split(delimiter);
+    }
+
+    private int[] toInts(String[] values) {
+        int[] toInts = new int[values.length];
+        for (int i = 0; i < values.length; i++) {
+            toInts[i] = toPositive(values[i]);
+        }
+        return toInts;
+    }
+
+    private int toPositive(String value) {
+        int positive;
+        positive = Integer.parseInt(value);
+        if(positive<0)
+            throw new RuntimeException();
+        return positive;
     }
 }
--- src/test/java/chap02/calculator/StringCalculatorTest.java
+++ src/test/java/chap02/calculator/StringCalculatorTest.java
@@ -1,14 +1,53 @@
 package chap02.calculator;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculatorTest {
+
+    private  StringCalculator cal;
+
+    @BeforeEach
+    public void setUp() {
+        cal = new StringCalculator();
+    }
+    
+    @Test
+    @DisplayName("숫자 하나 입력시 그대로 반환")
+    public void input_one_number_then_return_one() throws Exception{
+        //give
+        
+        //when
+        
+        //then
+        assertEquals(1,cal.sum("1"));
+        assertEquals(2,cal.sum("2"));
+        assertEquals(3,cal.sum("3"));
+    }
+
+//    @Test
+//    @DisplayName("문자 하나 입력시 그대로 반환")
+//    public void input_one_word_then_return_0() throws Exception{
+//        //give
+//
+//        //when
+//
+//        //then
+//        assertEquals(0,cal.sum("a"));
+//        assertEquals(0,cal.sum(","));
+//        assertEquals(0,cal.sum("("));
+//    }
+
     
     @Test
     public void sum_with_comma() throws Exception{
         //give
-        StringCalculator cal = new StringCalculator();
 
         //when
 
@@ -16,5 +55,72 @@ public class StringCalculatorTest {
         assertEquals(3, cal.sum("1,2"));
         assertEquals(5, cal.sum("2,3"));
     }
+
+    @Test
+    public void sum_with_colon() throws Exception{
+        //give
+
+        //when
+
+        //then
+        assertEquals(3, cal.sum("1:2"));
+        assertEquals(9, cal.sum("4:5"));
+    }
+
+    @Test
+    public void sum_with_colon_and_comma() throws Exception{
+        //give
+
+        //when
+
+        //then
+        assertEquals(6, cal.sum("1,2:3"));
+        assertEquals(15, cal.sum("4:5,6"));
+    }
+
+    @Test
+    @DisplayName("빈 입력일 경우 0반환")
+    public void input_nothing_then_0() throws Exception{
+        //give
+
+        //when
+
+        //then
+        assertEquals(0,cal.sum(""));
+    }
+
+    @Test
+    @DisplayName("커스텀 구분자 찾기")
+    public void get_custom_delimiter() {
+        //give
+        String s = "//;\n1;2,3";
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(s);
+        m.find();
+        String del = m.group(1);
+        assertEquals(";",del);
+    }
+
+    @Test
+    public void sum_with_colum_comma_and_customDelimeter() throws Exception{
+        //give
+        String input = "//#\n1,2:3#4";
+        //when
+
+        //then
+        assertEquals(10, cal.sum(input));
+    }
+
+    @Test
+    public void sum_with_negative_then_return0() throws Exception{
+        //give
+
+        //when
+
+        //then
+        assertThrows(RuntimeException.class, () -> {
+            cal.sum("1,-1,2");
+        });
+    }
+
     
 }
